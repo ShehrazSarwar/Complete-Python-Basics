9116fdd67cd4cff50fa25cdc45f38088627cd986
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-02-24 22:51:12 +0500

    Add files via upload

---
+++ File Processing/Basics.py
@@ -0,0 +1,6 @@
+# let's learn about file input and output
+# To open a file use open() function (by default it is set to read mode) open(file,r)
+# To open a file in write mode open(file,w)
+# open() returns a "file handle" - a variable used to perform operations on the file
+
+
